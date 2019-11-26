49632df9d12ea1f20a6aef0a3cd9586b2f51e557
jan <jcjvanroestel@gmail.com>
2019-11-25 19:58:31 -0800

    fix ebmodel etc

--- ztf_ebfit/ztflc.py
+++ ztf_ebfit/ztflc.py
@@ -5,7 +5,7 @@ import copy
 
 from .periodfind import run_BLScuvarbase
 from .utils import flux2mag, mag2flux
-from .ebmodels import EBmodel_multiband, ellc_WDRD
+from .ebmodels import EBmodel_multiband
 #import .lcmodels as lcmodels
 
 """import periodfind"""
@@ -255,104 +255,6 @@ class Ztflc:
 
 
 
-    def fit_ellc_WDRD(self,filters=[1,2]):
-
-        def log_likelihood(theta, x, y, yerr, fid, filters):
-
-            # set values
-            lnp = 0
-            basepars = theta[:7] # p,t0,r1,r2,incl,q,M1
-
-            for k,f in enumerate(filters): # for each band, use EBmodel
-                m = (fid==f) # fit data 
-                pars = np.r_[theta[:7],theta[7+k*4:7+(k+1)*4]]
-                log_f = pars[-1]
-                model = lcmodels.ellc_WDRD(pars[:-1],x[m],logpars=False)   
-                sigma2 = yerr[m]**2 + model**2*np.exp(2*log_f)
-                chi2 = np.sum((y[m]-model)**2/sigma2)
-                print("%g %g" %(chi2,np.sum(m)))
-                lnp += -0.5*chi2 + np.sum(np.log(sigma2))
-
-
-            # safety check
-            if np.isnan(lnp):
-                print(theta)
-                return -np.inf
-
-            return lnp
-
-        # select data
-        m = np.isin(self.fid,filters)
-        m *= (self.flag == 0)
-
-        # setup for fitting procedure
-        from scipy.optimize import minimize
-        nll = lambda *args: -log_likelihood(*args)
-        initial = [self.p,self.t0,0.05,0.1,89.9,0.5,0.6,]
-        bounds = [(self.p*1-10**-7,self.p*1+10**-7),
-                  (self.t0-self.p*0.05,self.t0+self.p*0.05),
-                  (0,0.5),(0,0.5),(0,90-10**-8),(0.01,1.5),(0,1.44)]
-        scales = [-9,-4,-2,-2,-1,-2,-2]
-        for f in filters:
-            med = np.median(self.y[self.fid==1])
-            initial += [med,0.001,0.5,-3.]    
-            bounds += [(0,None),(10**-10,None),(0,1),(-10,-1),]   
-            scales = [np.log10(med)-2,-10,-3,-1] 
-
-        # run fit
-        soln = minimize(nll, initial, bounds=bounds,
-            args=(self.t[m], self.y[m], self.dy[m], self.fid[m],filters))
-
-        # run quick emcee chain
-        """
-        pos = soln.x + 10**np.array(np.array(scales))*np.random.randn(32, len(initial))
-        nwalkers, ndim = pos.shape
-
-        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(x, y, yerr))
-        sampler.run_mcmc(pos, 100)
-        """
-
-        # store results
-        self.fit_ellc_WDRD_sol = soln
-        self.fit_ellc_WDRD_filters = filters
-
-
-    def plot_ellc_WDRD(self,theta=None,filters=None):
-        if theta is None:
-            theta = self.fit_ellc_WDRD_sol.x
-        if filters is None:
-            filters = self.fit_ellc_WDRD_filters
-
-        # phase folded data
-        p = theta[0]
-        t0 = theta[1]
-        x = (self.t-t0)/p%1
-
-
-        basepars = theta[:7] # p,t0,r1,r2,incl,q,M1
-        marker = 'o'
-        colours = {1:'C2',2:'C3',3:'k'}
-
-        mt = np.linspace(0,p,3000)    
-    
-        for k,f in enumerate(filters): # for each band, use EBmodel
-            c = colours[f]
-            m = (self.fid==f) # fit data 
-            pars = np.r_[theta[:7],theta[7+k*4:7+(k+1)*4]]
-            mlcs = ellc_WDRD(pars[:-1],mt)   
-            
-            plt.figure()
-
-            for n in [-1,0,1]:
-                plt.errorbar(x[m]+n,self.y[m],self.dy[m],
-                    marker=marker,c=c,ls='none')
-                plt.plot((mt-t0)/p%1+n,mlcs,'k-')
-
-                
-            plt.show()
-
-
-
     def plotlc(self,folded=False,mag=False,period=None,showflagged=True):
         # plot the lightcurve
         fig, ax1 = plt.subplots()
@@ -402,87 +304,9 @@ class Ztflc:
         plt.show()
 
 
-    def KPED_queue(self):
-        """ print a line of text to observe the eclipse for KPED
-
-        # relevant columns are
-"requestID", "programID", "objectID", "ra_hex", "dec_hex", "epoch", "ra_rate", "dec_rate", "mag", "exposure_time", "filter", "mode", "pi", "comment"
-
-        """    
-        
-        from astropy import units as u
-        from astropy.coordinates import SkyCoord
-        c = SkyCoord(ra=self.ra_med*u.degree, dec=self.dec_med*u.degree, 
-            frame='icrs')
-        c_hex = c.to_string('hmsdms')
-        c_hex = c_hex.replace('h',':')
-        c_hex = c_hex.replace('m',':')
-        c_hex = c_hex.replace('d',':')
-        c_hex = c_hex.replace('s','')
-
-        name = c.to_string('hmsdms')
-        name = name.replace('h','')
-        name = name.replace('m','')
-        name = name.replace('d','')
-        name = name.replace('s','')
-
-
-        requestID = "Eclipse_ZTFJ%s%s" %(name[:4],name.split()[1][:5])
-        programID = "1"
-        objectID = "ZTFJ%s%s" %(name[:4],name.split()[1][:5])
-        ra_hex = c_hex.split()[0]
-        dec_hex = c_hex.split()[1]
-        epoch = "2000.0"
-        ra_rate = "0.0"
-        dec_rate = "0.0"
-        mag = "%4.4g" %self.Rmag    
-        exposure_time = "%f" %(900.) # will be overwritten by eclipse duration in comment.
-        KPED_filter = "FILTER_SLOAN_R"
-        mode = "6"
-        pi = "VanRoestel"
-        window = (3.0*self.dur)*self.p # duration of eclipse in days
-        comment = "%12.12f_%12.12f_%12.12f" %(self.p,self.t0,window)
-
-        output = ','.join([requestID,programID,objectID,ra_hex,dec_hex,
-                    epoch,ra_rate,dec_rate,mag,exposure_time,KPED_filter,
-                    mode,pi,comment])
-
-        print(output)
 
 
 
 
 
-def check_timedobservation(ra,dec,p,t0,dtp,dtm,buffertime=15./60/24):
-
-    import numpy as np
-    from astropy import units as u
-    from astropy.time import Time
-    from astropy import coordinates as coord
-    from datetime import datetime
-
-    # calculate time of next eclipse
-    t0 = Time(t0,format='jd', scale='tdb') # assuming HJD
-
-    # get current time
-    nt = Time.now()
-    ut = Time(datetime.utcnow(), scale='utc')
-
-    # convert to heliocentre
-    ut_helio = JD2HJD(ut,ra,dec)
-
-    # calculate the number of eclipses since t0
-    N = (ut_helio-t0).value//p
-    
-    # calculate the time of the next upcoming eclipse in HJD time
-    _t_helio = t0+p*(N+1) # eclipse time at heliocentre
-    _t = HJD2JD(_t_helio,ra,dec)
-
-    dt = (_t - nt)
-
-    if dt < buffertime+dtp and dt>dtp:
-        # if the remaining is less than the buffertime + starttime and more than startime, start observation.
-        return True
-    else:
-        return false
 

b5172ccc38313cf8620966da9cfc85be7f86e1e9
jan <jcjvanroestel@gmail.com>
2019-12-02 00:55:07 -0800

    added emcee to trapfit

--- ztf_ebfit/ztflc.py
+++ ztf_ebfit/ztflc.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import minimize, least_squares, brute, basinhopping, curve_fit
+from scipy.optimize import minimize, least_squares, brute, basinhopping
 import copy
+import emcee
 
 from .periodfind import run_BLScuvarbase
 from .utils import flux2mag, mag2flux
@@ -146,29 +147,32 @@ class Ztflc:
             x_scale = np.r_[x_scale,np.array([0.1,0.1,0.1,0.01,0.01])]
 
         # RUN LSTQ FIT
-        #func = lambda pars: ((y-EBmodel_multiband(pars,t,fid))/dy)
-        #output = least_squares(func,x0,bounds=bounds.T,x_scale=x_scale,
-        #    verbose=verbose)
+        func = lambda pars: ((y-EBmodel_multiband(pars,t,fid))/dy)
+        output = least_squares(func,x0,bounds=bounds.T,x_scale=x_scale,
+            verbose=verbose)
 
-        #self.EBmodel['basepars'] = output.x[:4]
-        #for n,k in enumerate(filters):
-        #    self.EBmodel['filtpars'][k] = output.x[4+(n*5):4+((n+1)*5)]
-
-    
-        def f(t,*pars):
-            return EBmodel_multiband(pars,t,fid)
-
-        output = curve_fit(f,t,y,p0=x0,sigma=dy,bounds=bounds,absolute_sigma=True)
-
-        self.EBmodel['basepars'] = output.popt[:4]
+        self.EBmodel['basepars'] = output.x[:4]
         for n,k in enumerate(filters):
-            self.EBmodel['filtpars'][k] = output.popt[4+(n*5):4+((n+1)*5)]
+            self.EBmodel['filtpars'][k] = output.x[4+(n*5):4+((n+1)*5)]
 
+        self.EBmodel
 
         self.trap = dict()
         self.trap['output'] = output
         #self.trap['output2'] = output2.x
-        self.trap['my'] = EBmodel_multiband(output.popt,t,fid)
+        self.trap['my'] = EBmodel_multiband(output.x,t,fid)
+
+
+
+        if mcmc:
+            def lnp(pars):
+                print('running')
+                return -0.5*np.sum(func(pars)
+
+            nwalkers = 128
+            pos = output.x + x_scale * 10**-2 * np.random.randn(nwalkers, len(output.x))
+            sampler = emcee.EnsembleSampler(nwalkers, ndim, lnp,)
+            sampler.run_mcmc(pos, 100)                
 
 
 
